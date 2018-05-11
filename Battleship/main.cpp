cbe31485415a10aea96a1f0594c1c9050e00ed39
freddie <freddie@freddies-macbook-pro.local>
2018-05-11 15:19:57 +0500

    Initial Commit

---
+++ Battleship/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Battleship
+//
+//  Created by freddie on 5/11/18.
+//  Copyright © 2018 freddie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
